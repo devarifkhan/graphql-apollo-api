a9f6563dd579ee05b71b07ea60ef60304a488e03
devarifkhan <arifcse209@gmail.com>
2025-06-17 16:47:51 +0600

    feat: implement GraphQL server with greeting query

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-const express= require('express');
-const {ApolloServer} = require('apollo-server-express');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const cors = require('cors');
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+const typeDefs = gql`
+type Query{
+    greeting: String
+}`;
+
+const resolvers = {
+    Query: {
+        greeting: () => "Hello from GraphQL!"
+    }
+};
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/",(req,res,next)=>{
-    res.send("Hello World");
-});
+async function startServer() {
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, path: '/graphql' });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    app.use("/", (req, res, next) => {
+        res.send("Hello World");
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`GraphQL endpoint: http://localhost:${PORT}${apolloServer.graphqlPath}`);
+    });
+}
+
+startServer().catch(error => {
+    console.error('Error starting server:', error);
 });
 
